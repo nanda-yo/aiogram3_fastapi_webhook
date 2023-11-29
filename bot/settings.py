b45dbb1411b162e6d8c54438aefd03980d8913f3
meow <123@meow.com>
2023-11-30 02:10:14 +0300

    No changes

--- bot/settings.py
+++ bot/settings.py
@@ -4,7 +4,7 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 class Settings(BaseSettings):
     model_config = SettingsConfigDict(
         env_file = ('process.env','.env'),
-        ##env_file_encoding = 'utf-8'
+        #env_file_encoding = 'utf-8'
     )
     debug: bool=True
     bot_token: str = "default_bot_token_string"
