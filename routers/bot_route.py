13ca7e82c04139eef140f4ee78d84e6d2c4292c6
nanda-yo <104871983+nanda-yo@users.noreply.github.com>
2023-11-30 02:24:16 +0300

    fixed minor fuckup :D

--- routers/bot_route.py
+++ routers/bot_route.py
@@ -8,8 +8,9 @@ config = get_settings()
 
 tg_bot_router = APIRouter()
 
-@tg_bot_router.post(f"{config.webhook_path}")
-async def bot_webhook(update:dict,received_x_tg_token: Annotated[str|None,Header()] = None) -> None | dict:
+@tg_bot_router.post(config.webhook_path)
+async def bot_webhook(update: dict,
+                      received_x_tg_token: Annotated[str | None, Header()] = None) -> None | dict:
     if received_x_tg_token != config.secret_tg_token:
         print(f" Secret token doesnt match")
         return {f"Token mismatch, error."}
